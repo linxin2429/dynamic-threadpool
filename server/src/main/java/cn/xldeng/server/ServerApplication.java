f371920d05ddeb474fe49ea783ede5bf965ecf54
linxin2429 <dengxinlin2429@gmail.com>
2022-06-27 01:32:24 +0800

    Initial commit

---
+++ server/src/main/java/cn/xldeng/server/ServerApplication.java
@@ -0,0 +1,17 @@
+package cn.xldeng.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @program: threadpool
+ * @description:
+ * @author: dengxinlin
+ * @create: 2022-06-27 01:28
+ */
+@SpringBootApplication
+public class ServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
