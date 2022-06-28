175354c1bce97978281181ff45de124a49d95cd3
linxin2429 <dengxinlin2429@gmail.com>
2022-06-28 22:25:11 +0800

    xldeng feat: 增加listener

---
+++ dtp-spring-boot-starter/src/main/java/cn/xldeng/starter/listener/ClientWorker.java
@@ -0,0 +1,10 @@
+package cn.xldeng.starter.listener;
+
+/**
+ * @program: threadpool
+ * @description: 客户端监听
+ * @author: dengxinlin
+ * @create: 2022-06-28 18:04
+ */
+public class ClientWorker {
+}
