e0cf8b3996f02261a97f531f5e936b02724a6cf5
linxin2429 <dengxinlin2429@gmail.com>
2022-06-28 16:06:55 +0800

    xldeng feat: 添加常量类

---
+++ dtp-spring-boot-starter/src/main/java/cn/xldeng/starter/common/Constants.java
@@ -0,0 +1,17 @@
+package cn.xldeng.starter.common;
+
+/**
+ * @program: threadpool
+ * @description:
+ * @author: dengxinlin
+ * @create: 2022-06-27 07:31
+ */
+public class Constants {
+    public static final String DEFAULT_GROUP = "DEFAULT_GROUP";
+
+    public static final String DATA_ID = "dataId";
+
+    public static final String GROUP_ID = "group";
+
+    public static final String DEFAULT_NAMESPACE_ID = "public";
+}
