e2757cdbe8f659690b9c09c294ec9aa324a10e04
linxin2429 <dengxinlin2429@gmail.com>
2022-06-28 15:53:44 +0800

    xldeng feat: OkHttpClient 配置，https支持

---
+++ dtp-spring-boot-starter/src/main/java/cn/xldeng/starter/config/OkHttpClientConfig.java
@@ -0,0 +1,72 @@
+package cn.xldeng.starter.config;
+
+import cn.xldeng.starter.tookit.HttpClientUtil;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @program: threadpool
+ * @description:
+ * @author: dengxinlin
+ * @create: 2022-06-27 07:32
+ */
+@Slf4j
+@Configuration
+public class OkHttpClientConfig {
+    @Bean
+    public OkHttpClient okHttpClient() {
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        builder.connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(30, TimeUnit.SECONDS)
+                .build();
+        supportHttps(builder);
+        return builder.build();
+    }
+
+    @Bean
+    public HttpClientUtil httpClientUtil() {
+        return new HttpClientUtil();
+    }
+
+
+    /**
+     * 支持 Https
+     * @param builder OkHttpClient.Builder
+     */
+    @SneakyThrows
+    private void supportHttps(OkHttpClient.Builder builder) {
+        final TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[]{};
+            }
+        }};
+
+        final SSLContext sslContext = SSLContext.getInstance("SSL");
+        sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
+        final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
+        builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
+        builder.hostnameVerifier((hostname, session) -> true);
+    }
+}
