d16ecc5fc49d12181cb0abe73a5c87edadc3ef7e
linxin2429 <dengxinlin2429@gmail.com>
2022-06-29 22:58:54 +0800

    xldeng feat: Events

---
+++ dtp-server/src/main/java/cn/xldeng/server/event/LocalDataChangeEvent.java
@@ -0,0 +1,16 @@
+package cn.xldeng.server.event;
+
+/**
+ * @program: threadpool
+ * @description:
+ * @author: dengxinlin
+ * @create: 2022-06-29 16:30
+ */
+public class LocalDataChangeEvent extends Event {
+    private static final long serialVersionUID = 6467584761461884799L;
+    public final String groupKey;
+
+    public LocalDataChangeEvent(String groupKey) {
+        this.groupKey = groupKey;
+    }
+}
