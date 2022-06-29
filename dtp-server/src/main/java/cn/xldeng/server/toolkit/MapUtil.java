f48dcfa2ac800720bf885ceeed628ac96f8956aa
linxin2429 <dengxinlin2429@gmail.com>
2022-06-29 20:38:05 +0800

    xldeng feat: 工具类

---
+++ dtp-server/src/main/java/cn/xldeng/server/toolkit/MapUtil.java
@@ -0,0 +1,33 @@
+package cn.xldeng.server.toolkit;
+
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.BiFunction;
+
+/**
+ * @program: threadpool
+ * @description:
+ * @author: dengxinlin
+ * @create: 2022-06-29 16:33
+ */
+public class MapUtil {
+    public static Object computeIfAbsent(Map target,
+                                         Object key,
+                                         BiFunction mappingFunction,
+                                         Object param1,
+                                         Object param2) {
+        Objects.requireNonNull(target, "target");
+        Objects.requireNonNull(key, "key");
+        Objects.requireNonNull(mappingFunction, "mappingFunction");
+        Objects.requireNonNull(param1, "param1");
+        Objects.requireNonNull(param2, "param2");
+
+        Object val = target.get(key);
+        if (val == null) {
+            Object ret = mappingFunction.apply(param1, param2);
+            target.put(key, ret);
+            return ret;
+        }
+        return val;
+    }
+}
