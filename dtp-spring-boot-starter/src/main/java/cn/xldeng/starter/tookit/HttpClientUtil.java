ce47ee4c95a15128046fe5edc125b25fe8528121
linxin2429 <dengxinlin2429@gmail.com>
2022-06-29 13:25:09 +0800

    xldeng feat: HttpClientUtil添加包含headers和timeout参数的Get和Post方法

--- dtp-spring-boot-starter/src/main/java/cn/xldeng/starter/tookit/HttpClientUtil.java
+++ dtp-spring-boot-starter/src/main/java/cn/xldeng/starter/tookit/HttpClientUtil.java
@@ -5,9 +5,11 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.CollectionUtils;
 
 import java.nio.charset.StandardCharsets;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @program: threadpool
@@ -21,15 +23,15 @@ public class HttpClientUtil {
     @Autowired
     private OkHttpClient okHttpClient;
 
-    private MediaType jsonMediaType = MediaType.parse("application/json; charset=utf-8");
+    private final MediaType jsonMediaType = MediaType.parse("application/json; charset=utf-8");
 
     private static final int HTTP_OK_CODE = 200;
 
     /**
      * Get 请求
      *
-     * @param url
-     * @return
+     * @param url url
+     * @return response
      */
     @SneakyThrows
     public String get(String url) {
@@ -44,21 +46,21 @@ public class HttpClientUtil {
     /**
      * Get 请求, 支持添加查询字符串
      *
-     * @param url
+     * @param url         url
      * @param queryString 查询字符串
-     * @return
+     * @return response
      */
-    public String get(String url, Map<String, Object> queryString) {
-        String fullUrl = queryString(url, queryString);
+    public String get(String url, Map<String, String> queryString) {
+        String fullUrl = buildUrl(url, queryString);
         return get(fullUrl);
     }
 
     /**
      * 获取 Json 后直接反序列化
      *
-     * @param url
-     * @param clazz
-     * @return
+     * @param url   url
+     * @param clazz class type
+     * @return response
      */
     public <T> T restApiGet(String url, Class<T> clazz) {
         String resp = get(url);
@@ -68,14 +70,14 @@ public class HttpClientUtil {
     /**
      * Get 请求, 支持查询字符串
      *
-     * @param url
-     * @param queryString
-     * @param clazz
-     * @param <T>
-     * @return
+     * @param url url
+     * @param queryString 查询字符串
+     * @param clazz class
+     * @param <T> 泛型
+     * @return response
      */
-    public <T> T restApiGet(String url, Map<String, Object> queryString, Class<T> clazz) {
-        String fullUrl = queryString(url, queryString);
+    public <T> T restApiGet(String url, Map<String, String> queryString, Class<T> clazz) {
+        String fullUrl = buildUrl(url, queryString);
         String resp = get(fullUrl);
         return JSON.parseObject(resp, clazz);
     }
@@ -85,7 +87,7 @@ public class HttpClientUtil {
      *
      * @param url  url
      * @param body request body
-     * @return
+     * @return response
      */
     public String restApiPost(String url, Object body) {
         try {
@@ -102,7 +104,7 @@ public class HttpClientUtil {
      *
      * @param url  url
      * @param body request body
-     * @return
+     * @return response
      */
     public <T> T restApiPost(String url, Object body, Class<T> clazz) {
         String resp = restApiPost(url, body);
@@ -114,15 +116,15 @@ public class HttpClientUtil {
      *
      * @param url         url
      * @param queryString 查询字符串
-     * @return
+     * @return new url
      */
-    public String queryString(String url, Map<String, Object> queryString) {
+    public String buildUrl(String url, Map<String, String> queryString) {
         if (null == queryString) {
             return url;
         }
         StringBuilder builder = new StringBuilder(url);
         boolean isFirst = true;
-        for (Map.Entry<String, Object> entry : queryString.entrySet()) {
+        for (Map.Entry<String, String> entry : queryString.entrySet()) {
             String key = entry.getKey();
             if (key != null && entry.getValue() != null) {
                 if (isFirst) {
@@ -166,5 +168,60 @@ public class HttpClientUtil {
         }
         assert response.body() != null;
         return response.body().bytes();
+    }
+
+    @SneakyThrows
+    public <T> T restApiGetByThreadPool(String url,
+                                        Map<String, String> headers,
+                                        Map<String, String> paramValues,
+                                        Long readTimeout,
+                                        Class<T> clazz) {
+        String buildUrl = buildUrl(url, paramValues);
+        Request.Builder builder = new Request.Builder().get();
+
+        if (!CollectionUtils.isEmpty(headers)) {
+            builder.headers(Headers.of(headers));
+        }
+
+        Request request = builder.url(buildUrl).build();
+
+        Call call = okHttpClient.newCall(request);
+        call.timeout().timeout(readTimeout, TimeUnit.MILLISECONDS);
+
+        Response response = call.execute();
+        if (response.code() != HTTP_OK_CODE) {
+            String msg = String.format("HttpGet 响应 code 异常. [code] %s [url] %s", response.code(), url);
+            log.error(msg);
+            throw new RuntimeException(msg);
+        }
+        assert response.body() != null;
+        return JSON.parseObject(response.body().string(), clazz);
+    }
+
+    @SneakyThrows
+    public <T> T restApiPostByThreadPool(String url,
+                                         Map<String, String> headers,
+                                         Map<String, String> paramValues,
+                                         Long readTimeout,
+                                         Class<T> clazz) {
+        String buildUrl = buildUrl(url, paramValues);
+
+        Request request = new Request.Builder()
+                .url(buildUrl)
+                .headers(Headers.of(headers))
+                .post(RequestBody.create(jsonMediaType, ""))
+                .build();
+
+        Call call = okHttpClient.newCall(request);
+        call.timeout().timeout(readTimeout, TimeUnit.MILLISECONDS);
+
+        Response response = call.execute();
+        if (response.code() != HTTP_OK_CODE) {
+            String msg = String.format("HttpPost 响应 code 异常. [code] %s [url] %s", response.code(), url);
+            log.error(msg);
+            throw new RuntimeException(msg);
+        }
+        assert response.body() != null;
+        return JSON.parseObject(response.body().string(), clazz);
     }
 }
