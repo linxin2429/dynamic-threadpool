9bee13b91ccb30a05a7d867f2ed34dcfeaabfe8f
linxin2429 <dengxinlin2429@gmail.com>
2022-06-30 16:04:02 +0800

    xldeng feat: 状态枚举

---
+++ dtp-server/src/main/java/cn/xldeng/server/enums/DelEnum.java
@@ -0,0 +1,38 @@
+package cn.xldeng.server.enums;
+
+/**
+ * @program: threadpool
+ * @description:
+ * @author: dengxinlin
+ * @create: 2022-06-30 15:54
+ */
+public enum DelEnum {
+    /**
+     * 正常状态
+     */
+    NORMAL("0"),
+
+    /**
+     * 删除状态
+     */
+    DELETE("1");
+
+    private final String statusCode;
+
+    DelEnum(String statusCode) {
+        this.statusCode = statusCode;
+    }
+
+    public String getCode() {
+        return this.statusCode;
+    }
+
+    public Integer getIntCode() {
+        return Integer.parseInt(this.statusCode);
+    }
+
+    @Override
+    public String toString() {
+        return statusCode;
+    }
+}
