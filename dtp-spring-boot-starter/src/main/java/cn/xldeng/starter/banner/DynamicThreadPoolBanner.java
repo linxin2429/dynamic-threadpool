81b718e2c9a2c9bc0c2a3bd3200c411d11364183
linxin2429 <dengxinlin2429@gmail.com>
2022-07-01 08:35:55 +0800

    xldeng feat: 添加banner

---
+++ dtp-spring-boot-starter/src/main/java/cn/xldeng/starter/banner/DynamicThreadPoolBanner.java
@@ -0,0 +1,43 @@
+package cn.xldeng.starter.banner;
+
+import org.springframework.boot.ansi.AnsiColor;
+import org.springframework.boot.ansi.AnsiOutput;
+import org.springframework.boot.ansi.AnsiStyle;
+
+/**
+ * @program: threadpool
+ * @description:
+ * @author: dengxinlin
+ * @create: 2022-06-30 17:56
+ */
+public class DynamicThreadPoolBanner {
+    private static final String DYNAMIC_THREAD_POOL = " :: Dynamic ThreadPool :: ";
+
+    private static final int STRAP_LINE_SIZE = 50;
+
+    public static void printBanner(boolean isBanner) {
+        String banner = "\n___                       _      _____ ___ \n" +
+                "|   \\ _  _ _ _  __ _ _ __ (_)__  |_   _| _ \\\n" +
+                "| |) | || | ' \\/ _` | '  \\| / _|   | | |  _/\n" +
+                "|___/ \\_, |_||_\\__,_|_|_|_|_\\__|   |_| |_|  \n" +
+                "      |__/                                  \n";
+
+        if (isBanner) {
+            String version = getVersion();
+            version = (version != null) ? " (v" + version + ")" : "no version.";
+
+            StringBuilder padding = new StringBuilder();
+            while (padding.length() < STRAP_LINE_SIZE - (version.length() + DYNAMIC_THREAD_POOL.length())) {
+                padding.append(" ");
+            }
+
+            System.out.println(AnsiOutput.toString(banner, AnsiColor.GREEN, DYNAMIC_THREAD_POOL, AnsiColor.DEFAULT,
+                    padding.toString(), AnsiStyle.FAINT, version, "\n"));
+        }
+    }
+
+    public static String getVersion() {
+        final Package pkg = DynamicThreadPoolBanner.class.getPackage();
+        return pkg != null ? pkg.getImplementationVersion() : "";
+    }
+}
